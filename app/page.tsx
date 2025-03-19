ea9956c990c3e9cec80c9980b2584301ef9e91ff
TharakaJayz <tharakaprabhath300@gmail.com>
2025-03-20 01:11:42 +0530

    added hero-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import HeroSection from "@/components/home/hero-section";
 import { Button } from "@/components/ui/button";
+import BgGradient from "@/components/ui/common/bg-gradient";
 
 export default function Home() {
   return (
     <div className="relative w-full">
+      <BgGradient/>
+      <div className="flex flex-col">
+
       <HeroSection />
+      </div>
       {/* <DemoSection />
       <HowItWorksSection />
       <PricingSection />
