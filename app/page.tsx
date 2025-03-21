12e088e6e11719ab1e91faf8b32de13f752e899b
TharakaJayz <tharakaprabhath300@gmail.com>
2025-03-21 20:56:36 +0530

    demo-section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
+import DemoSection from "@/components/home/demo-section";
 import HeroSection from "@/components/home/hero-section";
-import { Button } from "@/components/ui/button";
 import BgGradient from "@/components/ui/common/bg-gradient";
+import HowItWorksSection from "@/components/ui/common/how-its-works";
 
 export default function Home() {
   return (
     <div className="relative w-full">
-      <BgGradient/>
+      <BgGradient />
       <div className="flex flex-col">
-
-      <HeroSection />
+        <HeroSection />
+        <DemoSection />
+        <HowItWorksSection />
       </div>
-      {/* <DemoSection />
-      <HowItWorksSection />
+      {/*
+     
       <PricingSection />
       <CTASection /> */}
     </div>
