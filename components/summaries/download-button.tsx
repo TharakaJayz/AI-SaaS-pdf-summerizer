4c6638d28d40a1d0dada6787f5b6ed9bc55b13f4
TharakaJayz <tharakaprabhath300@gmail.com>
2025-05-08 10:43:35 +0530

    added single summary view

--- components/summaries/download-button.tsx
+++ components/summaries/download-button.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from '../ui/button'
+import { Download } from 'lucide-react'
 
 type Props = {
     title:string,
@@ -9,8 +10,13 @@ type Props = {
 }
 
 const DownloadSummaryButton = (props: Props) => {
+  const handleDownload = () =>{
+    
+  }
   return (
-    <Button size={"sm"} className='h-8 px-3 bg-rose-100 text-rose-600 hover:text-rose-700 hover:bg-rose-50'>DownloadSummaryButton</Button>
+    <Button size={"sm"} className='h-8 px-3 bg-rose-100 text-rose-600 hover:text-rose-700 hover:bg-rose-50' onClick={handleDownload}>
+      <Download />
+      Download Summary</Button>
   )
 }
 
